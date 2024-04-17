8575204838474b3d941bbce1a299138dd6f5f8bd
Ilya R. Gorbatenko <irgorbatenko-d@rttv.ru>
2024-04-17 12:57:15 +0300

    red

--- Program.cs
+++ Program.cs
@@ -1,2 +1,17 @@
-﻿
-Console.WriteLine("hi ");
+﻿int val = 100;
+long vall = 2*100500;
+double b = 6.6;
+char c = '!';
+bool tr = true;
+string text = "tect";
+
+
+
+Console.WriteLine(c);
+Console.WriteLine(b);
+
+Console.WriteLine(val);
+Console.WriteLine(tr);
+
+Console.WriteLine(text);
+Console.WriteLine(vall);
