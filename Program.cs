ec7602474f2ffb960d0b86c77b3f539f98821fba
Илья <166902999+LonleyLokley@users.noreply.github.com>
2024-04-21 16:29:22 +0300

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -27,6 +27,51 @@ else {
     Console.WriteLine(true);
 }
 
+See https://aka.ms/new-console-template for more informatstr
+
+Console.Write("Введите перое число");
+int num1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите второе число");
+int num2 = Convert.ToInt32(Console.ReadLine());
+
+if (num1 == num2 * num2)
+{
+ Console.WriteLine(true);
+}
+else
+{
+ Console.WriteLine(false);
+}
+
+
+
+Console.Write("Enter a numder ");
+int n =  Convert.ToInt32(Console.ReadLine());
+int count = -n; //n*(-1)
+while (count<=n)
+{
+Console.Write(count + " ");
+ count++;
+}
+
+
+
+
+
+int n =  Convert.ToInt32(Console.ReadLine());
+if (n>=100 && n<=999) {
+    int furst = n/100;
+int last = n % 10;
+int res = furst+last;
+
+
+Console.WriteLine($"result is {res}");
+}
+
+else {
+    Console.Write($"the num {n} ni too math");
+}
+
 
 
 
